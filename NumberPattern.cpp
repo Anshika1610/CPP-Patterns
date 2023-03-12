66b62d47b0f33a5c14beb87812ebb162131a5a82
Anshika1610 <71184320+Anshika1610@users.noreply.github.com>
2023-03-12 20:45:32 +0530

    Create NumberPattern.cpp

---
+++ NumberPattern.cpp
@@ -0,0 +1,19 @@
+void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<j<<" ";
+            }
+            for(int j=0;j<=2*n-2*i;j++){
+                cout<<" ";
+            }
+            for(int j=2*n-2*i;j>0;j--){
+                cout<<" ";
+            }
+            for(int j=i;j>=1;j--){
+                cout<<j<<" ";
+            }
+
+            cout<<endl;
+        }
+    }
